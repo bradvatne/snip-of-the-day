92d70c72506aa80de99e8bf2c0d92b6db1aed858
bradvatne <brad.vatne@gmail.com>
2023-04-25 18:19:32 +0800

    actually so confused

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import supabase from "../lib/supabase";
-import "server-only";
 import SnipPreview from "../components/SnipPreview";
 
 export default async function Home() {
-  const { data: snips, error } = await supabase.from("snips").select();
-  error ? console.log(error) : console.log(snips);
-  return snips?.map((snip, index) => (
-    <SnipPreview key={index} snippet={snip} />
-  ));
+  const { data, error } = await supabase.from("snips").select();
+  return (
+    <div>
+      {data &&
+        data.map((snip, index) => <SnipPreview key={index} snippet={snip} />)}
+    </div>
+  );
 }
